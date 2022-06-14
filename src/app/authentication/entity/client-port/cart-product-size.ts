d61e8c4028c2250cbc3bfa966bb7a22c1602721e
NPHUNG <hung929912a@gmail.com>
2022-06-14 22:04:05 +0700

    [client] add shopping cart

---
+++ src/app/authentication/entity/client-port/cart-product-size.ts
@@ -0,0 +1,9 @@
+export class CartProductSize {
+    id: number;
+    productSizeId: number;
+    userId: number;
+    count: number;
+    address: String;
+    status: String;
+    createAt: Date;
+}
