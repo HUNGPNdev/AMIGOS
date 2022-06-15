592a4c340ebc0ac495326419be34367b7c7f2a5b
NPHUNG <hung929912a@gmail.com>
2022-06-15 17:26:23 +0700

    [client] add shopping cart

--- src/app/authentication/test-service/shopping-cart/shopping-cart.component.ts
+++ src/app/authentication/test-service/shopping-cart/shopping-cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { CartProductSize } from '../../entity/client-port/cart-product-size';
+import { ClientPortService } from '../../entity/client-port/client-port.service';
 
 @Component({
   selector: 'app-shopping-cart',
@@ -6,10 +8,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-cart.component.css']
 })
 export class ShoppingCartComponent implements OnInit {
-
-  constructor() { }
+  cartProductSize: CartProductSize = new CartProductSize();
+  cartProductSizes: CartProductSize[];
+  constructor(private clientPortService: ClientPortService) { }
 
   ngOnInit(): void {
+    this.getCartByUser();
   }
 
+  getCartByUser() {
+    this.clientPortService.getCartByUser().subscribe( data => {
+      this.cartProductSizes = data.data;
+      console.log(this.cartProductSizes)
+    }, error => console.log(error))
+  }
 }
