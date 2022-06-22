33c3720a5bb5f180c1f17de642af10ab241ddb8c
NPHUNG <hung929912a@gmail.com>
2022-06-22 17:53:34 +0700

    [BE] update docker

--- src/main/java/com/amigos/client/ImageController.java
+++ src/main/java/com/amigos/client/ImageController.java
@@ -1,14 +1,26 @@
 package com.amigos.client;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.ResourceUtils;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.constraints.NotEmpty;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
 
 @RestController
+@RequestMapping("/images/")
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class ImageController {
 
-//    @GetMapping("/images/images")
-//    public ResponseEntity<ResponseApi> getImages(@NotEmpty @PathVariable("userName") String userName) {
-//        return ResponseEntity.ok(orderService.findAllByUserId(httpServletRequest));
-//    }
+    @GetMapping("{images}")
+    public byte[] getImages(@NotEmpty @PathVariable("images") String images) throws IOException {
+        File file = ResourceUtils.getFile("/images/" + images);
+        System.out.println("File:"+file.getName());
+        if(file.exists()) {
+            byte[] fileData = Files.readAllBytes(file.toPath());
+            return fileData;
+        }
+        return null;
+    }
 }
--- src/main/java/com/amigos/product/impl/ProductServiceImpl.java
+++ src/main/java/com/amigos/product/impl/ProductServiceImpl.java
@@ -14,6 +14,7 @@ import com.amigos.user.model.User;
 import com.amigos.user.repository.UserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -62,6 +63,9 @@ public class ProductServiceImpl implements ProductService {
 
     @Autowired
     EntityManager entityManager;
+
+    @Autowired
+    private Environment properties;
 
     @Override
     @Transactional
@@ -158,11 +162,20 @@ public class ProductServiceImpl implements ProductService {
 
     private void setProductImages(MultipartFile image_1, MultipartFile image_2, MultipartFile image_3, ProductEntity entity) throws IOException
     {
+        String env = properties.getProperty("spring.datasource.url");
+        String rootPath = "";
+        if(env.equals("jdbc:mysql://localhost:3306/amigos?useSSL=false")) {
+            rootPath = "D:/amigos/images/";
+        } else if (env.equals("jdbc:mysql://mysqldb/amigos?allowPublicKeyRetrieval=true&useSSL=false")){
+            rootPath = "images/";
+        } else {
+            return;
+        }
         Date date = new Date();
         createFolderIfNotExit();
         if(image_1 != null) {
             String fileName = date.getTime()+"1"+image_1.getOriginalFilename();
-            Path location = Paths.get("images/" + fileName);
+            Path location = Paths.get(rootPath + fileName);
             try {
                 Files.copy(image_1.getInputStream(),
                         location,
@@ -174,7 +187,7 @@ public class ProductServiceImpl implements ProductService {
         }
         if(image_2 != null) {
             String fileName = date.getTime()+"2"+image_2.getOriginalFilename();
-            Path location = Paths.get("images/" + fileName);
+            Path location = Paths.get(rootPath + fileName);
             try {
                 Files.copy(image_2.getInputStream(),
                         location,
@@ -186,7 +199,7 @@ public class ProductServiceImpl implements ProductService {
         }
         if(image_3 != null) {
             String fileName = date.getTime()+"3"+image_3.getOriginalFilename();
-            Path location = Paths.get("images/" + fileName);
+            Path location = Paths.get(rootPath + fileName);
             try {
                 Files.copy(image_3.getInputStream(),
                         location,
